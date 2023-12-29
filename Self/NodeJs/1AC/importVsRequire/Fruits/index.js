766c27e12817412463eaecee57f2fc14b8812b38
Md Faizuddin <imdfaizuddin@gmail.com>
2023-12-29 21:46:57 +0530

    understanding require and import

---
+++ Self/NodeJs/1AC/importVsRequire/Fruits/index.js
@@ -0,0 +1,11 @@
+// const  apple = require("./apple");
+// const  banana = require("./banana");
+// const  mango = require("./mango");
+
+import apple from "./apple";
+import banana from "./banana";
+import mango from "./mango";
+
+export const arr = [
+    apple , banana , mango
+];
