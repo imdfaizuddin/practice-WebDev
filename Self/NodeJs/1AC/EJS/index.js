65be06bd1c7b27d66fb6d994d3d3e9defd93c343
Md Faizuddin <imdfaizuddin@gmail.com>
2023-12-30 19:00:39 +0530

    learning EJS

--- Self/NodeJs/1AC/EJS/index.js
+++ Self/NodeJs/1AC/EJS/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.set("view engine", "ejs");
+
+app.get("/", (req,res)=>{
+    res.render("home.ejs");
+});
+
 app.listen(port, ()=>{
     console.log("listning to port:",port);
 });
