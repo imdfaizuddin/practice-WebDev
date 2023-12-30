4c20513826c5692a59b62fc5704078c17b2a9cfd
Md Faizuddin <imdfaizuddin@gmail.com>
2023-12-30 20:18:29 +0530

    added app.get('/rolldice'

--- Self/NodeJs/1AC/EJS/index.js
+++ Self/NodeJs/1AC/EJS/index.js
@@ -9,7 +9,10 @@ app.set("view engine", "ejs");
 app.get("/", (req,res)=>{
     res.render("home.ejs");
 });
-
+app.get("/rolldice", (req,res)=>{
+    let diceVal = Math.floor((Math.random()*6))+1;
+    res.render("rollDice.ejs", {diceVal});
+});
 app.listen(port, ()=>{
     console.log("listning to port:",port);
 });
