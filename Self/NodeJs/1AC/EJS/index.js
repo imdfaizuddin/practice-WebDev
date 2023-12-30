8a9eb2e75087a72a9939d6b454c44b4ce0e1abcb
Md Faizuddin <imdfaizuddin@gmail.com>
2023-12-30 19:23:03 +0530

    setting views relative  path

--- Self/NodeJs/1AC/EJS/index.js
+++ Self/NodeJs/1AC/EJS/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
+app.set("views", path.join(__dirname, "/views"));
 
 app.set("view engine", "ejs");
 
