c04e104e341771d7e36e60e5043fdaeb39b9d277
Md Faizuddin <imdfaizuddin@gmail.com>
2023-12-31 19:45:19 +0530

    learning EJS

--- Self/NodeJs/1AC/EJS/index.js
+++ Self/NodeJs/1AC/EJS/index.js
@@ -13,6 +13,20 @@ app.get("/rolldice", (req,res)=>{
     let diceVal = Math.floor((Math.random()*6))+1;
     res.render("rollDice.ejs", {diceVal});
 });
+
+app.get("/ig/:username", (req,res)=>{
+    let instaData = require("./data.json");
+    let { username } = req.params;
+    let data = instaData[username];         //sends data only for :username
+    // console.log(data);
+    // const followers = ["Adam", "Bro", "cate", "Daniel"];
+    // res.render("instagram.ejs", {username , followers});
+    if(data){
+    res.render("instagram.ejs", {data});
+    }else{
+        res.render("instaError.ejs");
+    }
+});
 app.listen(port, ()=>{
     console.log("listning to port:",port);
 });
