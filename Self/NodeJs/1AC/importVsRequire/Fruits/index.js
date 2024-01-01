4a82929143d14c98f327e4361c99f64223f1981b
Md Faizuddin <imdfaizuddin@gmail.com>
2024-01-01 22:15:30 +0530

    learning import and require in NODE js

--- Self/NodeJs/1AC/importVsRequire/Fruits/index.js
+++ Self/NodeJs/1AC/importVsRequire/Fruits/index.js
@@ -2,10 +2,15 @@
 // const  banana = require("./banana");
 // const  mango = require("./mango");
 
-import apple from "./apple";
-import banana from "./banana";
-import mango from "./mango";
 
-export const arr = [
-    apple , banana , mango
-];
+
+// Import won't work because npm donsen't have type: module
+import {objapple} from "./apple.js";
+const arr = [objapple];
+import {objbanana} from "./banana.js";
+arr.push(objbanana);
+import {objmango} from "./mango.js";
+arr.push(objmango);
+
+
+export let index = arr;
