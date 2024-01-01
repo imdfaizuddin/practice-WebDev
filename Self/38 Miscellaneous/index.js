afc6fdc03e58edbf5f739beb9facc49516dffef8
Md Faizuddin <imdfaizuddin@gmail.com>
2024-01-01 22:08:02 +0530

    learning GET & POST req handling

---
+++ Self/38 Miscellaneous/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/register", (req,res)=>{
+    let {user , password} = req.query;
+    res.send(`standard GET response. welcome ${user}! Your password is ${password}`);
+});
+
+app.post("/register", (req,res)=>{
+    // res.send(`Post request detected`);
+    let {user , password} = req.body;
+    console.log(req.body);
+    res.send(`Welcome ${user}!`);
+});
+
+app.listen(port, ()=>{
+    console.log(`listening to port: ${port}`);
+})
