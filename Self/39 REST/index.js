64e846846a55420b262f74618d27a99ca01f1bb8
Md Faizuddin <imdfaizuddin@gmail.com>
2024-01-05 23:14:20 +0530

    added get /posts path

--- Self/39 REST/index.js
+++ Self/39 REST/index.js
@@ -2,6 +2,35 @@ const express = require("express");
 const app = express();
 
 const port = 8080;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname,"public")));
+
+let posts = [
+    {
+        username : "Adil",
+        content : "I love coding"
+    },
+
+    {
+        username : "Dua",
+        content : "Hard work is important"
+    },
+
+    {
+        username : "Kaif",
+        content : "I am really cool"
+    },
+]
+app.get("/posts", (req,res)=>{
+    res.render("index.ejs", { posts });
+});
+
+app.get("/", (req, res)=>{
+    res.send("welcome to root");
+});
 
 app.listen(port, ()=>{
     console.log("app is listning to port:", port);
