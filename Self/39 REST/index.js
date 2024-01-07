cc896ccc3eb1147c9561a44efab25a22a84fd626
Md Faizuddin <imdfaizuddin@gmail.com>
2024-01-07 15:56:52 +0530

    added new post adding function

--- Self/39 REST/index.js
+++ Self/39 REST/index.js
@@ -6,7 +6,9 @@ const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
 
 let posts = [
     {
@@ -26,6 +28,20 @@ let posts = [
 ]
 app.get("/posts", (req,res)=>{
     res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new",(req, res)=>{
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req,res)=>{
+    // console.log(req.body);
+    // res.send("post req working");
+    // posts.push(req.body);
+    let {username , content} = req.body;
+    posts.push({username, content});
+    console.log(username);
+    res.redirect("/posts");
 });
 
 app.get("/", (req, res)=>{
