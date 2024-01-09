675bd9b9b8d59987fb00a3364169c08eb15ba178
Md Faizuddin <imdfaizuddin@gmail.com>
2024-01-09 17:38:52 +0530

    added view edit and delete posts

--- Self/39 REST/index.js
+++ Self/39 REST/index.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname,"/public")));
@@ -31,20 +33,21 @@ let posts = [
         content : "I am really cool"
     },
 ]
+
+// Showing all posts
 app.get("/posts", (req,res)=>{
     res.render("index.ejs", { posts });
 });
 
+// Display add new post page
 app.get("/posts/new",(req, res)=>{
     res.render("new.ejs");
 });
-
+// post req of new.ejs form
 app.post("/posts", (req,res)=>{
-    // console.log(req.body);
-    // res.send("post req working");
-    // posts.push(req.body);
+    let id = uuidv4();
     let {username , content} = req.body;
-    posts.push({username, content});
+    posts.push({username, content , id});
     res.redirect("/posts");
 });
 
@@ -53,6 +56,28 @@ app.get("/posts/:id", (req,res)=>{
     // console.log(id);
     let post = posts.find((p)=> id === p.id);
     res.render("view.ejs", { post });
+});
+//edit get request
+app.get("/posts/:id/edit", (req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=> id === p.id);
+    // console.log(id);
+    res.render("edit.ejs", {post});
+});
+// patch request
+app.patch("/posts/:id", (req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    // console.log(newContent);
+    let post = posts.find((p)=> id === p.id);
+    post.content = newContent;
+    // console.log(post);
+    res.redirect("/posts");
+});
+app.delete("/posts/:id", (req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p)=> id != p.id);
+    res.redirect("/posts");
 });
 app.get("/", (req, res)=>{
     res.send("welcome to root");
