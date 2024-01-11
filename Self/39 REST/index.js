c97ba0eaff567587afad3457d75e00a6f66ab6a2
Md Faizuddin <imdfaizuddin@gmail.com>
2024-01-11 22:53:21 +0530

    updated index.js

--- Self/39 REST/index.js
+++ Self/39 REST/index.js
@@ -3,15 +3,16 @@ const app = express();
 
 const port = 8080;
 const path = require("path");
-
+const { v4: uuidv4 } = require('uuid');
 const methodOverride = require('method-override');
-app.use(methodOverride('_method'));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
+
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,"/public")));
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
-const { v4: uuidv4 } = require('uuid');
 
 
 let posts = [
@@ -36,10 +37,10 @@ let posts = [
 
 // Showing all posts
 app.get("/posts", (req,res)=>{
-    res.render("index.ejs", { posts });
+    res.render("index.ejs", { posts }); 
 });
 
-// Display add new post page
+// add new post page
 app.get("/posts/new",(req, res)=>{
     res.render("new.ejs");
 });
