7d044feb3c200b44b465bdf7ed0d12e1a1d613ef
Md Faizuddin <imdfaizuddin@gmail.com>
2024-01-18 20:09:04 +0530

    added 101 users data using faker in SQL database table

--- Self/42 SQL1/index.js
+++ Self/42 SQL1/index.js
@@ -1,4 +1,4 @@
-const { faker } = require('@faker-js/faker');
+const { faker, da } = require('@faker-js/faker');
 const mysql = require('mysql2');
 
 
@@ -9,8 +9,38 @@ const connection = mysql.createConnection({
     database: 'instagram',
     password: "2022"
   });
-
-  connection.query("SHOW TABLES", (err, result)=>{
+  // query for selecting all(*) fields from table student where marks is 70 using placeholders 
+  let q = `SELECT *
+  FROM student
+  WHERE marks >= ?
+  `
+  let user = [70];
+  // query for creating table in SQL database
+let createTable = `CREATE TABLE user(
+    id VARCHAR(50) PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL
+    )`;
+// adding 100 users in user table using faker
+let getUser = ()=> {
+        return [
+          faker.string.uuid(),
+          faker.internet.userName(),
+          faker.internet.email(),
+          faker.internet.password(),
+        ];
+ }
+// query for adding 100 values using placeholder ? and 2d array [data]
+let inputVal = `INSERT INTO user (id ,username, email, password) VALUES ?`;
+  
+    let data = [];
+    for(let i=0; i<100; i++){
+      data.push(getUser());
+    }
+    console.log(data);
+  
+  connection.query(inputVal, [data], (err, result)=>{
     try {
         if(err) throw err;
         console.log(result);
@@ -19,14 +49,8 @@ const connection = mysql.createConnection({
     }
   });
 
+  connection.end();
 
-let getRandomUser = ()=> {
-    return {
-      id: faker.string.uuid(),
-      username: faker.internet.userName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-  }
-
-//   console.log(getRandomUser());
+  // let data = [];
+  // data.push(getRandomUser());
+  // console.log(data);
