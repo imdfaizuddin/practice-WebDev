24465d0bc56009fa4a29fe87c52ae9d74837ef44
Md Faizuddin <imdfaizuddin@gmail.com>
2024-01-29 23:48:30 +0530

    add user and emp models with users and employees collections

--- Self/Mongoose/index.js
+++ Self/Mongoose/index.js
@@ -2,10 +2,19 @@ const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-main().then(res =>{
-    console.log("connection successful")
+main().then(res => {
+  console.log("connection successful")
 }).catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
 }
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+// const User = mongoose.model("user", userSchema);
+// const Emp = mongoose.model("employees", userSchema);
