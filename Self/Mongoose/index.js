88229d1e0806b174bcd225309af9be5e6ad2555c
Md Faizuddin <imdfaizuddin@gmail.com>
2024-01-31 21:11:49 +0530

    add user2 in User model using  mongoose in js file

--- Self/Mongoose/index.js
+++ Self/Mongoose/index.js
@@ -18,6 +18,13 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("user", userSchema);
 // const Emp = mongoose.model("employees", userSchema);
-const user1 = new User({name: "Adil", email: "adil@gmail.com", age: 21});  //user1 obj or document
+// const user1 = new User({name: "Adil", email: "adil@gmail.com", age: 21});  //user1 obj or document
 
-user1.save();  //to save in db & returns a promise
+// user1.save();  //to save in db & returns a promise
+
+const user2 = new User({name: "Eve", email: "eve@gamil.com", age: 22});
+user2.save().then((res)=>{
+  console.log(res);
+}).catch((err)=>{
+  console.log(err);
+});
