a6ac51d910ff3ea518b29268fde8544faddd8f8e
Md Faizuddin <imdfaizuddin@gmail.com>
2024-01-31 21:34:04 +0530

    insertMany in User model using  mongoose in js file

--- Self/Mongoose/index.js
+++ Self/Mongoose/index.js
@@ -16,15 +16,23 @@ const userSchema = new mongoose.Schema({
   age: Number,
 });
 
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model("User", userSchema);
 // const Emp = mongoose.model("employees", userSchema);
 // const user1 = new User({name: "Adil", email: "adil@gmail.com", age: 21});  //user1 obj or document
 
 // user1.save();  //to save in db & returns a promise
 
-const user2 = new User({name: "Eve", email: "eve@gamil.com", age: 22});
-user2.save().then((res)=>{
+// const user2 = new User({name: "Eve", email: "eve@gamil.com", age: 22});
+// user2.save().then((res)=>{
+//   console.log(res);
+// }).catch((err)=>{
+//   console.log(err);
+// });
+
+User.insertMany([
+  {name:"Tony", email: "tony@gmail.com", age: 55},
+  {name: "Bruce", email: "bruce@gmail.com", age: 47},
+  {name: "Peter", email: "peter@gmail.com", age: 30},
+]).then((res)=>{
   console.log(res);
-}).catch((err)=>{
-  console.log(err);
-});
+}).catch((err)=>{ console.log(err)});
