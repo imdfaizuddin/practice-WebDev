450f9f11bac90afd554f68f798ae1796821950ef
Md Faizuddin <imdfaizuddin@gmail.com>
2024-02-01 23:57:33 +0530

    add & use Teacher class obj

--- Self/38 Miscellaneous/inheritance.js
+++ Self/38 Miscellaneous/inheritance.js
@@ -25,11 +25,17 @@ class Teacher extends Person{
         this.subject = subject;
     }
     getSubject(){
-        console.log(`${this.subject}`)
+        // console.log(`${this.subject}`);
+        return this.subject;
     }
 }
 
 let stu1 = new Student("Adil", 21 , 98);
+let tea1 = new Teacher("Amy", 24, "English");
 
 console.log(stu1.name);
 stu1.talk();
+
+tea1.getSubject();
+console.log(`hi! my name is ${tea1.name} I teach ${tea1.subject} to students.`);
+console.log(`hi! my name is ${tea1.name} I teach ${tea1.getSubject()} to students.`);
