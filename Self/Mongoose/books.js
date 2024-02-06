49adc6abaa1980affd8fa1532a48ff0b7ede8309
Md Faizuddin <imdfaizuddin@gmail.com>
2024-02-07 00:19:53 +0530

    define bookSchema mongoose

--- Self/Mongoose/books.js
+++ Self/Mongoose/books.js
@@ -7,3 +7,16 @@ main().then(res=>{
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/flipkart');
 }
+
+const bookSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+    },
+    price:{
+        type: Number,
+    },
+});
