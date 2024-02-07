123f61f2b622fb618b042d3c0910e6e60a30330e
Md Faizuddin <imdfaizuddin@gmail.com>
2024-02-07 22:30:28 +0530

    findbyIdAndUpdate & runValidators

--- Self/Mongoose/books.js
+++ Self/Mongoose/books.js
@@ -12,25 +12,45 @@ const bookSchema = mongoose.Schema({
     title: {
         type: String,
         required: true,
+        trim: true,
     },
     author: {
         type: String,
     },
     price:{
         type: Number,
+        min : [1 , "Please enter valid price"],
+    },
+    discount: {
+        type: Number,
+        default: 0,
+    },
+    genre: [String],
+
+    category: {
+        type: String,
+        enum: ["fiction", "non-fiction"],
     },
 });
 
 const Book = mongoose.model("Book", bookSchema);
 
-const book1 = new Book({
-    title: "Mathematics XII",
-    author: "RD Sharma",
-    price : 1200
-});
+// const book1 = new Book({
+//     title: "  Wit   ",
+//     author:" MacHale",
+//     price : 1099,
+//     discount: 20,
+//     genre: ["comedy", "Non-fiction", "Humor"],
+//     category: "non-fiction",
+// });
 
-book1.save().then(res=>{
+// book1.save().then(res=>{
+//     console.log(res);
+// }).catch(err=>{
+//     console.log("error is :- ",err);
+// });
+Book.findByIdAndUpdate("65c3a4f36cc76714783deddc", {discount: 20},{runValidators: true}).then(res=>{
     console.log(res);
 }).catch(err=>{
-    console.log(err);
+    console.log("error is: ", err);
 });
