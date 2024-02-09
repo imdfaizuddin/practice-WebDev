fd4ef763ff0703e9dc3e7fdb10a8a78b0adc77e4
Md Faizuddin <imdfaizuddin@gmail.com>
2024-02-09 23:41:51 +0530

    basic setup mongoose connectiion

--- Self/46 MongoDB with Express (chat)/index.js
+++ Self/46 MongoDB with Express (chat)/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
-
-
+const mongoose = require("mongoose");
 const PORT = 3000;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+main().then(()=>{
+    console.log("connection successful")
+}).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+
+}
+
+app.get("/", (req,res)=>{
+    res.send("working");
+});
 app.listen(PORT, ()=>{
     console.log("server is listening to port: ", PORT);
 });
