da4dd6fdc0b9157476362d883eaf7cb61fa91b2a
Md Faizuddin <imdfaizuddin@gmail.com>
2024-02-15 15:20:45 +0530

    add first chat in database

--- Self/46 MongoDB with Express (chat)/index.js
+++ Self/46 MongoDB with Express (chat)/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const PORT = 3000;
 const path = require("path");
+const Chat = require("./models/chat.js")
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -15,6 +16,17 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 
 }
+
+const chat1 = new Chat({
+    from: "Neha",
+    to: "Priya",
+    msg: "Send class notes",
+    created_at: new Date()
+});
+
+chat1.save().then(res =>{
+    console.log(res);
+}).catch(err => {console.log("Error is:", err)});
 
 app.get("/", (req,res)=>{
     res.send("working");
