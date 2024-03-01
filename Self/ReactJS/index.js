4f6f65b9a00ff11bad9e07d6d8a770bcd1e783d5
Md Faizuddin <imdfaizuddin@gmail.com>
2024-03-01 23:50:14 +0530

    Keeper app homePage

--- Self/ReactJS/index.js
+++ Self/ReactJS/index.js
@@ -1,8 +1,98 @@
 
 
 
+// ====================================================================================================================================
+// Keeper App homepage
+// Index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-====================================================================================================================================
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
+
+//1. Create a new React app.
+//2. Create a App.jsx component.
+//3. Create a Header.jsx component that renders a <header> element
+//to show the Keeper App name in an <h1>.
+//4. Create a Footer.jsx component that renders a <footer> element
+//to show a copyright message in a <p> with a dynamically updated year.
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
+
+// App.jsx
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import {Note} from "./Note";
+
+function App(){
+    return(
+        <div>
+        <Header />
+        <Note />
+        <Footer />
+        </div>
+    );
+};
+
+export default App;
+
+// Header.jsx
+import React from "react";
+
+function Header(){
+    return (<header>
+        
+        <h1>Keeper</h1>
+        
+    </header>);
+};
+
+export default Header;
+
+//Footer.jsx
+import React from "react";
+
+
+
+function Footer() {
+    const year = new Date().getFullYear();
+    console.log(year);
+    return (
+        <footer>
+            <p>&copy; {year}</p>
+        </footer>
+    )
+}
+
+
+export default Footer;
+
+//Note.jsx
+import React from "react";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1>This is note title</h1>
+            <p>This is note content</p>
+        </div>
+    )
+}
+
+export default Note;
+export {Note};
+
+
+
+// ====================================================================================================================================
 // IMPORT/ EXPORT PRACTICE
 // Index.Js
 import React from "react";
