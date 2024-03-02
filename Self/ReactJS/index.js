038b648097e79874af08f49fd658b6e79e9a701a
Md Faizuddin <imdfaizuddin@gmail.com>
2024-03-02 18:40:23 +0530

    React Hooks useState

--- Self/ReactJS/index.js
+++ Self/ReactJS/index.js
@@ -3,6 +3,33 @@
 
 
 // =============================================================================================================================
+// 306- React Hooks useState 
+// App.jsx:
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+// =============================================================================================================================
 // 304 -Conditional Rendering Practice 
 // App.js: 
 import React from "react";
