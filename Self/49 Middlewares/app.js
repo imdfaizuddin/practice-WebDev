ddf1054571ba71ca2ac9e14ca6d7e18f7dd45d09
Md Faizuddin <imdfaizuddin@gmail.com>
2024-03-18 23:42:40 +0530

    middlewares

---
+++ Self/49 Middlewares/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+
+// App.use is a middleware its always executed when request is sent to any path
+app.use((req,res,next)=>{
+    let {method, path, responseTime,hostname} = req;
+    console.log("hi, i am middleware")
+    console.log("time: ",new Date(Date.now()).toString())
+    console.log(method, path,responseTime,hostname)
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log("I am 2nd middleware");
+    next();
+})
+app.get("/", (req,res)=>{
+    res.send("root");
+});
+
+app.get("/random", (req,res)=>{
+    res.send("Random Route");
+});
+
+app.listen(3000, ()=>{
+    console.log("server is listning to port 3000");
+})
